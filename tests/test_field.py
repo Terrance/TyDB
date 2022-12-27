e5919b9cf9c55b0f2e645644b19facf068d44c4d
Terrance <git@terrance.allofti.me>
2022-12-27 23:14:13 +0000

    Add field tests for StrField

--- tests/test_field.py
+++ tests/test_field.py
@@ -3,7 +3,7 @@ import time
 from typing import Union
 from unittest import TestCase
 
-from tydb.fields import IntField
+from tydb.fields import IntField, StrField
 from tydb.models import Table
 from tydb.session import AsyncSession, Session
 from tydb.utils import maybe_await
@@ -35,7 +35,7 @@ class TestIntField(TestCase):
     async def test_field_select(self, sess: Union[AsyncSession, Session]):
         await maybe_await(sess.create(self.Model, field=self.main_value))
         result = [inst async for inst in await maybe_await(sess.select(self.Model))]
-        self.assertEqual(len(result), self.main_value)
+        self.assertEqual(len(result), 1)
         self.assertEqual(result[0], self.Model(field=self.main_value))
 
     async def test_field_select_where(self, sess: Union[AsyncSession, Session]):
@@ -43,7 +43,54 @@ class TestIntField(TestCase):
         result = [inst async for inst in await maybe_await(sess.select(
             self.Model, +self.Model.field == self.main_value,
         ))]
-        self.assertEqual(len(result), self.main_value)
+        self.assertEqual(len(result), 1)
+        result = [inst async for inst in await maybe_await(sess.select(
+            self.Model, +self.Model.field == self.alt_value,
+        ))]
+        self.assertEqual(len(result), 0)
+
+    async def test_field_remove(self, sess: Union[AsyncSession, Session]):
+        inst = await maybe_await(sess.create(self.Model, field=self.main_value))
+        if inst is None:
+            self.skipTest("Returning instance not supported by driver")
+        await maybe_await(sess.remove(inst))
+        result = [inst async for inst in await maybe_await(sess.select(self.Model))]
+        self.assertEqual(len(result), 0)
+
+    async def test_field_delete(self, sess: Union[AsyncSession, Session]):
+        await maybe_await(sess.create(self.Model, field=self.main_value))
+        await maybe_await(sess.delete(self.Model, self.main_value))
+        result = [inst async for inst in await maybe_await(sess.select(self.Model))]
+        self.assertEqual(len(result), 0)
+
+
+@with_dialects
+class TestStrField(TestCase):
+
+    class Model(Table, name="model{}".format(next(index)), primary="field"):
+        field = StrField(size=8)
+
+    main_value = "main"
+    alt_value = "alt"
+
+    async def test_field_create(self, sess: Union[AsyncSession, Session]):
+        inst = await maybe_await(sess.create(self.Model, field=self.main_value))
+        if inst is None:
+            self.skipTest("Returning instance not supported by driver")
+        self.assertEqual(inst, self.Model(field=self.main_value))
+
+    async def test_field_select(self, sess: Union[AsyncSession, Session]):
+        await maybe_await(sess.create(self.Model, field=self.main_value))
+        result = [inst async for inst in await maybe_await(sess.select(self.Model))]
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0], self.Model(field=self.main_value))
+
+    async def test_field_select_where(self, sess: Union[AsyncSession, Session]):
+        await maybe_await(sess.create(self.Model, field=self.main_value))
+        result = [inst async for inst in await maybe_await(sess.select(
+            self.Model, +self.Model.field == self.main_value,
+        ))]
+        self.assertEqual(len(result), 1)
         result = [inst async for inst in await maybe_await(sess.select(
             self.Model, +self.Model.field == self.alt_value,
         ))]
